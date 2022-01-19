e5ee189fb2964fc4b215eea62e88f2df8a7249c1
Christian <christian.marquez@lgusuite.com>
2022-01-19 14:52:50 +0800

    add transaction/folder/document endpoint in API service

--- src/app/pages/portals/barangay-portal/pages/document-receiving/config.ts
+++ src/app/pages/portals/barangay-portal/pages/document-receiving/config.ts
@@ -1,11 +1,19 @@
 import { Column } from 'src/app/models/column.interface';
 
-// export const DOCUMENT_RECEIVING_TABLE :Column[] = [
-//     {
-//         title: 'Document Control #',
-//     breakpoint: 'sm',
-//     path: 'controlNumber',
-//     type: 'text',
-//     selected: true,
-//     }
-// ]
+export const DOCUMENT_RECEIVING_TABLE: Column[] = [
+  //   {
+  //     title: 'Transaction Reference Code',
+  //     breakpoint: 'sm',
+  //     path: 'refCode',
+  //     type: 'text',
+  //     selected: true,
+  //   },
+  {
+    title: 'Owner',
+    breakpoint: 'sm',
+    path: 'sender',
+    paths: ['sender.firstName', 'sender.lastName'],
+    type: 'text',
+    selected: true,
+  },
+];
--- src/app/pages/portals/barangay-portal/pages/document-receiving/document-receiving.component.ts
+++ src/app/pages/portals/barangay-portal/pages/document-receiving/document-receiving.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Column } from 'src/app/models/column.interface';
+import { QueryParams } from 'src/app/models/queryparams.interface';
+import { TableOutput } from 'src/app/models/tableemit.interface';
 import { ApiService } from 'src/app/service/api/api.service';
 import { UtilService } from 'src/app/service/util/util.service';
+import { DOCUMENT_RECEIVING_TABLE } from './config';
 
 @Component({
   selector: 'app-document-receiving',
@@ -10,9 +13,14 @@ import { UtilService } from 'src/app/service/util/util.service';
   styleUrls: ['./document-receiving.component.scss'],
 })
 export class DocumentReceivingComponent implements OnInit {
-  datasource: Array<any> = [];
-  columns: Column[] = [];
-  dataLenght: number = 0;
+  dataSource: Array<any> = [];
+  columns: Column[] = DOCUMENT_RECEIVING_TABLE;
+  dataLength: number = 0;
+  page: any = {
+    pageSize: 10,
+    pageIndex: 1,
+  };
+  loading: boolean = false;
 
   constructor(
     private api: ApiService,
@@ -20,5 +28,36 @@ export class DocumentReceivingComponent implements OnInit {
     private util: UtilService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchData(this.page);
+  }
+
+  fetchData(event: TableOutput) {
+    this.dataSource = [];
+    this.loading = true;
+    this.page.pageIndex = event.pageIndex;
+    this.page.pageSize = event.pageSize;
+    let query: QueryParams = {
+      find: [],
+      page: event.pageIndex,
+      limit: event.pageSize + '',
+    };
+    if (event.filter) query.filter = event.filter;
+    if (event.sort) {
+      query.sort =
+        (event.sort.direction === 'asc' ? '' : '-') + event.sort.active;
+    }
+
+    this.api.transaction.getAll(query).subscribe(
+      (res: any) => {
+        this.dataSource = res.env.transactions;
+        this.dataLength = res.total;
+        this.loading = false;
+      },
+      (error: any) => {
+        console.log(error);
+        this.loading = false;
+      }
+    );
+  }
 }
