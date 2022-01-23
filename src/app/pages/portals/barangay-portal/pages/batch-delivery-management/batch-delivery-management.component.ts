1e769fca3efc94b5ee79aecc91369f85ed0c0cc8
Leonard Galora <leonard.galora@lgusuite.com>
2022-01-23 13:19:38 +0800

    udpate viewing of document added viewer for images

--- src/app/pages/portals/barangay-portal/pages/batch-delivery-management/batch-delivery-management.component.ts
+++ src/app/pages/portals/barangay-portal/pages/batch-delivery-management/batch-delivery-management.component.ts
@@ -1,3 +1,4 @@
+import { DropboxService } from './../../../../../service/dropbox/dropbox.service';
 import { ActionResultComponent } from './../../../../../shared/dialogs/action-result/action-result.component';
 import { AreYouSureComponent } from './../../../../../shared/dialogs/are-you-sure/are-you-sure.component';
 import { ApiService } from './../../../../../service/api/api.service';
@@ -7,6 +8,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ViewDocumentComponent } from 'src/app/shared/components/view-document/view-document.component';
 import { RegistrantFormComponent } from 'src/app/shared/components/registrant-form/registrant-form.component';
+import { ViewVideoComponent } from 'src/app/shared/components/view-video/view-video.component';
 
 @Component({
   selector: 'app-batch-delivery-management',
@@ -39,7 +41,8 @@ export class BatchDeliveryManagementComponent implements OnInit {
     private api: ApiService,
     private dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private dbx: DropboxService
   ) {}
 
   ngOnInit(): void {
@@ -145,6 +148,21 @@ export class BatchDeliveryManagementComponent implements OnInit {
         break;
       case 'viewTransac':
         this.onViewTransac(event.obj);
+        break;
+      case 'viewVid':
+        this.dbx.getTempLink(event.obj.videoOfSignature.path_display).subscribe(
+          (res: any) => {
+            this.dialog.open(ViewVideoComponent, {
+              width: '50%',
+              disableClose: true,
+              data: { video: res.result.link, header: 'Video of Signing' },
+            });
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+
         break;
       default:
     }
