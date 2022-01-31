7e3c3b2c2d6dcb2fbc0d4bccd0eda600d3bd9022
unknown <virgilio@lgusuite.com>
2022-01-31 17:53:33 +0800

    channel name update

---
+++ src/app/pages/portals/notary-portal/pages/video-conferencing/room/config.ts
@@ -0,0 +1,27 @@
+export const USER_INFO = [
+  {
+    label: 'First Name',
+    path: 'firstName',
+  },
+  {
+    label: 'Middle Name',
+    path: 'middleName',
+  },
+  {
+    label: 'Last Name',
+    path: 'lastName',
+  },
+  {
+    label: 'Suffix',
+    path: 'suffix',
+  },
+  {
+    label: 'Birth date',
+    path: 'birthDate',
+    type: 'date',
+  },
+  {
+    label: 'Sex',
+    path: 'gender',
+  },
+];
--- src/app/pages/portals/notary-portal/pages/video-conferencing/room/room.component.ts
+++ src/app/pages/portals/notary-portal/pages/video-conferencing/room/room.component.ts
@@ -1,3 +1,5 @@
+import { USER_INFO } from './config';
+import { DropboxService } from './../../../../../../service/dropbox/dropbox.service';
 import { AuthService } from './../../../../../../service/auth/auth.service';
 import { AgoraService } from './../../../../../../service/api/agora/agora.service';
 import { Populate } from 'src/app/models/queryparams.interface';
@@ -5,11 +7,13 @@ import { ConferenceService } from './../../../../../../service/api/conference/co
 import { UtilService } from 'src/app/service/util/util.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { WebcamInitError } from 'ngx-webcam';
 import { Socket } from 'ngx-socket-io';
 import { Store } from '@ngrx/store';
 import { User } from 'src/app/models/user.interface';
+import { NgxCaptureService } from 'ngx-capture';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-room',
@@ -17,6 +21,7 @@ import { User } from 'src/app/models/user.interface';
   styleUrls: ['./room.component.scss'],
 })
 export class RoomComponent implements OnInit {
+  @ViewChild('screen', { static: true }) screen: any;
   // TODO: Create Interface for Schedule
   // TODO: Create Interface for Folder(Batch)
   // TODO: Create Interface for Transactions
@@ -38,7 +43,23 @@ export class RoomComponent implements OnInit {
   currentTransactionIndex = -1;
   transactions: any = [];
   currentTransaction: any;
+  _images: any = [
+    {
+      label: '1st Valid Government ID',
+      fcname: 'government_ID_1',
+    },
+    {
+      label: '2nd Valid Government ID',
+      fcname: 'government_ID_2',
+    },
+    {
+      label: 'Certificate of Indigency',
+      fcname: 'cert_of_indigency',
+    },
+  ];
+  userInfo = USER_INFO;
 
+  currentDocument: any;
   constructor(
     public dialogRef: MatDialogRef<RoomComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -46,9 +67,10 @@ export class RoomComponent implements OnInit {
     public util: UtilService,
     private conference: ConferenceService,
     private agora: AgoraService,
-    private auth: AuthService,
+    private dbx: DropboxService,
     private socket: Socket,
-    private store: Store<{ user: User }>
+    private store: Store<{ user: User }>,
+    private captureService: NgxCaptureService
   ) {}
 
   ngOnInit(): void {
@@ -92,6 +114,7 @@ export class RoomComponent implements OnInit {
   joinMeeting(schedule: any) {
     console.log(schedule);
     this.currentSchedule = schedule;
+    console.log(this.currentSchedule._id);
     this.transactions = [];
     this.currentSchedule._folderIds.forEach((folder: any) => {
       folder._transactions.forEach((transaction: any) => {
@@ -127,7 +150,61 @@ export class RoomComponent implements OnInit {
 
   nextTransaction() {
     this.currentTransactionIndex++;
+    this.initiateTransaction();
+  }
+
+  prevTransaction() {
+    this.currentTransactionIndex--;
+    this.initiateTransaction();
+  }
+
+  async initiateTransaction() {
     this.currentTransaction = this.transactions[this.currentTransactionIndex];
     console.log(this.currentTransaction);
+    this._images.forEach(async (image: any) => {
+      console.log(this.currentTransaction.sender[image.fcname]);
+      if (
+        this.currentTransaction.sender.images &&
+        this.currentTransaction.sender.images[image.fcname]
+      )
+        image.url = await this.getTempLink(
+          this.currentTransaction.sender.images[image.fcname].path_display
+        );
+      else delete image.url;
+    });
+    if (this.currentTransaction.videoOfSignature.path_display)
+      this.currentTransaction.vidURL = await this.getTempLink(
+        this.currentTransaction.videoOfSignature.path_display
+      );
+    else delete this.currentTransaction.vidURL;
+
+    this.selectDocumentToView(this.currentTransaction._documents[0]);
+
+    console.log(this.currentTransaction);
+  }
+
+  selectDocumentToView(event: any) {
+    console.log(event);
+    this.currentDocument = event;
+  }
+  takeScreenshot() {
+    this.captureService
+      .getImage(this.screen.nativeElement, true)
+      .pipe(
+        tap((img) => {
+          console.log(img);
+        })
+      )
+      .subscribe();
+  }
+
+  async getTempLink(data: any) {
+    console.log(data);
+    const response = await this.dbx.getTempLink(data).toPromise();
+    console.log(response);
+    return response.result.l;
+  }
+  saveImage(img: string) {
+    console.log(img);
   }
 }
