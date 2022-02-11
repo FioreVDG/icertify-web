983d807afff8ff633b706fdb65bdcd01254674d8
Leonard Galora <leonard.galora@lgusuite.com>
2022-02-11 16:06:06 +0800

    update final push for batch delivery

--- src/app/service/api/cluster/cluster.service.ts
+++ src/app/service/api/cluster/cluster.service.ts
@@ -23,4 +23,7 @@ export class ClusterService {
   delete(id: String) {
     return this.http.start('delete', `/clusters/${id}`);
   }
+  getOne(_brgyId: String) {
+    return this.http.start('get', `/clusters/getOne/${_brgyId}`);
+  }
 }
