fd5597616a9d8259bfc31f9152bd3ae7ea009609
Christian <christian.marquez@lgusuite.com>
2022-02-21 17:40:28 +0800

    update barangay vide conference table

--- src/app/pages/portals/barangay-portal/pages/barangay-video-conferencing/config.ts
+++ src/app/pages/portals/barangay-portal/pages/barangay-video-conferencing/config.ts
@@ -1,34 +1,221 @@
 import { Column } from 'src/app/models/column.interface';
+import { FILTER_BUTTON_COLUMN } from 'src/app/models/filter-button-conf.interface';
+import { Find } from 'src/app/models/queryparams.interface';
 
-export const CONFERENCE_TABLE: Column[] = [
+export const VIDEO_CONF_BARANGAY_TABLE: Array<FILTER_BUTTON_COLUMN> = [
   {
-    title: 'Schedule Id',
-    breakpoint: 'sm',
-    path: '_id',
-    type: 'text',
+    label: 'Pending',
     selected: true,
+    populate: [
+      {
+        field: '_notaryId',
+        select: 'firstName,lastName,middleName',
+      },
+    ],
+    column: [
+      {
+        title: 'Document Reference Code',
+        breakpoint: 'sm',
+        path: 'refCode',
+        type: 'text',
+        selected: true,
+      },
+      {
+        title: 'Document Title',
+        breakpoint: 'sm',
+        path: 'documentName',
+        type: 'text',
+        selected: true,
+      },
+      {
+        title: 'Document Type',
+        breakpoint: 'sm',
+        path: 'documentType',
+        type: 'text',
+        selected: true,
+        useAsFilter: true,
+        choices: [
+          'Power of Attorney',
+          'Medical Records',
+          'Sworn Statements',
+          'Affidavit',
+          'Deeds',
+          'Wills and Trusts',
+          'Others',
+        ],
+      },
+      {
+        title: 'QC Indigent',
+        breakpoint: 'sm',
+        path: 'sender',
+        paths: ['sender.firstName', 'sender.lastName'],
+        type: 'special',
+        selected: true,
+      },
+      {
+        title: 'Notary',
+        breakpoint: 'sm',
+        path: '_notaryId',
+        paths: ['_notaryId.firstName', '_notaryId.lastName'],
+        type: 'special',
+        selected: true,
+      },
+      // PAGAWA :)
+      // {
+      //   title: 'Queue No.',
+      //   breakpoint: 'sm',
+      //   path: '',
+      //   type: 'text',
+      //   selected: true,
+      // },
+      {
+        title: 'Scheduled By',
+        breakpoint: 'sm',
+        path: '_transactionId._scheduledBy',
+        paths: [
+          '_transactionId._folderId._scheduledBy.firstName',
+          '_transactionId._folderId._scheduledBy.lastName',
+        ],
+        type: 'special',
+        selected: true,
+      },
+      // PAGAWA :)
+      // {
+      //   title: 'Schedule',
+      //   breakpoint: 'sm',
+      //   path: '',
+      //   type: 'date',
+      //   selected: true,
+      // },
+    ],
   },
   {
-    title: 'Notary Name',
-    breakpoint: 'sm',
-    path: 'sender',
-    paths: ['_notaryId.firstName', '_notaryId.lastName'],
-    type: 'special',
-    selected: true,
-  },
+    label: 'Finished',
+    selected: false,
+    populate: [
+      {
+        field: '_notaryId',
+        select: 'firstName,lastName,middleName',
+      },
+    ],
 
+    column: [
+      {
+        title: 'Document Reference Code',
+        breakpoint: 'sm',
+        path: 'refCode',
+        type: 'text',
+        selected: true,
+      },
+      {
+        title: 'Document Title',
+        breakpoint: 'sm',
+        path: 'documentName',
+        type: 'text',
+        selected: true,
+      },
+      {
+        title: 'Document Type',
+        breakpoint: 'sm',
+        path: 'documentType',
+        type: 'text',
+        selected: true,
+        useAsFilter: true,
+        choices: [
+          'Power of Attorney',
+          'Medical Records',
+          'Sworn Statements',
+          'Affidavit',
+          'Deeds',
+          'Wills and Trusts',
+          'Others',
+        ],
+      },
+      {
+        title: 'QC Indigent',
+        breakpoint: 'sm',
+        path: 'sender',
+        paths: ['sender.firstName', 'sender.lastName'],
+        type: 'special',
+        selected: true,
+      },
+      {
+        title: 'Notary',
+        breakpoint: 'sm',
+        path: '_notaryId',
+        paths: ['_notaryId.firstName', '_notaryId.lastName'],
+        type: 'special',
+        selected: true,
+      },
+      // PAGAWA :)
+      // {
+      //   title: 'Queue No.',
+      //   breakpoint: 'sm',
+      //   path: '',
+      //   type: 'text',
+      //   selected: true,
+      // },
+      {
+        title: 'Scheduled By',
+        breakpoint: 'sm',
+        path: '_transactionId._scheduledBy',
+        paths: [
+          '_transactionId._folderId._scheduledBy.firstName',
+          '_transactionId._folderId._scheduledBy.lastName',
+        ],
+        type: 'special',
+        selected: true,
+      },
+      // PAGAWA :)
+      // {
+      //   title: 'Schedule',
+      //   breakpoint: 'sm',
+      //   path: '',
+      //   type: 'date',
+      //   selected: true,
+      // },
+      {
+        title: 'Status',
+        breakpoint: 'sm',
+        path: 'documentStatus',
+        type: 'text',
+        selected: true,
+        textColor: [
+          {
+            value: 'Notarized',
+            color: '#83b9a1' || 'green',
+          },
+          {
+            value: 'Unnotarized',
+            color: '#83b9a1' || 'green',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const PENDING_FIND: Find[] = [
   {
-    title: 'Schedule',
-    breakpoint: 'sm',
-    path: 'schedule',
-    type: 'date',
-    selected: true,
+    field: 'documentStatus',
+    operator: '[in]=',
+    value: 'Pending for Notary,Skipped',
   },
   {
-    title: 'Conference Status',
-    breakpoint: 'sm',
-    path: 'conferenceStatus',
-    type: 'text',
-    selected: true,
+    field: 'locationStatus',
+    operator: '[in]=',
+    value: 'Video Conference Scheduled (Notary)',
+  },
+];
+export const FINISHED_FIND: Find[] = [
+  {
+    field: 'documentStatus',
+    operator: '[in]=',
+    value: 'Notarized,Unnotarized',
+  },
+  {
+    field: 'locationStatus',
+    operator: '[nin]=',
+    value: 'Enroute to Barangay',
   },
 ];
