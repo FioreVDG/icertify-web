d60f22af6e4c46cda563a5d87aa421ccc5438783
Christian <christian.marquez@lgusuite.com>
2022-03-24 17:08:46 +0800

    update scheduled view transaction table

--- src/app/pages/portals/notary-portal/pages/video-conferencing/config.ts
+++ src/app/pages/portals/notary-portal/pages/video-conferencing/config.ts
@@ -1,3 +1,4 @@
+import { Column } from 'src/app/models/column.interface';
 import { FILTER_BUTTON_COLUMN } from 'src/app/models/filter-button-conf.interface';
 
 export const FILT_BTN: Array<FILTER_BUTTON_COLUMN> = [
@@ -113,4 +114,82 @@ export const FILT_BTN: Array<FILTER_BUTTON_COLUMN> = [
       },
     ],
   },
+];
+
+export const SCHEDULED_COLUMNS: Array<Column> = [
+  {
+    title: 'Document Reference Code',
+    breakpoint: 'sm',
+    path: 'refCode',
+    type: 'text',
+    selected: true,
+  },
+  {
+    title: 'Document Title',
+    breakpoint: 'sm',
+    path: 'documentName',
+    type: 'text',
+    selected: true,
+  },
+  {
+    title: 'Document Type',
+    breakpoint: 'sm',
+    path: 'documentType',
+    type: 'text',
+    selected: true,
+    useAsFilter: true,
+    choices: [
+      'Special Power of Attorney (SPA)',
+      'Affidavit',
+      'Brgy. Compromise Agreement',
+      'Quitclaim',
+      'Others',
+    ],
+  },
+  {
+    title: 'QC Indigent',
+    breakpoint: 'sm',
+    path: 'sender',
+    paths: ['sender.firstName', 'sender.lastName'],
+    type: 'special',
+    selected: true,
+  },
+  {
+    title: 'Queue No.',
+    breakpoint: 'sm',
+    path: 'queue',
+    type: 'number',
+    selected: true,
+  },
+  {
+    title: 'Schedule',
+    breakpoint: 'sm',
+    path: 'schedule',
+    type: 'date',
+    selected: true,
+  },
+  // {
+  //   title: 'Uploaded by',
+  //   breakpoint: 'sm',
+  //   path: '_createdBy',
+  //   paths: ['_createdBy.firstName', '_createdBy.lastName'],
+  //   type: 'special',
+  //   selected: true,
+  // },
+  // {
+  //   title: 'Date and Time Received',
+  //   breakpoint: 'sm',
+  //   path: 'updatedAt',
+
+  //   type: 'date',
+  //   selected: true,
+  // },
+  // {
+  //   title: 'No. of Documents',
+  //   breakpoint: 'sm',
+  //   path: 'documentCount',
+
+  //   type: 'text',
+  //   selected: true,
+  // },
 ];
